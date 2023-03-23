21d92786fcba340e74b607258b12019982f07b94
BRobinson404 <brobinso123@gmail.com>
2023-03-23 07:33:06 -0500

    declared newItem
    declared variables for list item and input value added alert if input field is left blank.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //adding a new item to the list and alert for no text in input box
 function newItem(){
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
+};
+
